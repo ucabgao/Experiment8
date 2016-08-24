c7deb284ea47f4046a5180dad80618ca864562c3
simon.gaozheng <simon.gaozheng@gmail.com>
2016-08-24 05:13:02 +0100

    annotation

--- chigley/rightmove-enhancement-suite/Rightmove_Enhancement_Suite.user.ts
+++ chigley/rightmove-enhancement-suite/Rightmove_Enhancement_Suite.user.ts
@@ -8,38 +8,48 @@
 // @grant       GM_getResourceText
 // @resource    style style.css
 // ==/UserScript==
+
 var $ = unsafeWindow.jQuery;
+
 GM_addStyle(GM_getResourceText("style"));
+
 // From https://stackoverflow.com/questions/4217962/scroll-to-an-element-using-jquery
-$.fn.scrollMinimal = function () {
+$.fn.scrollMinimal = function() {
     var cTop = this.offset().top;
     var cHeight = this.outerHeight(true);
     var windowTop = $(window).scrollTop();
     var visibleHeight = $(window).height();
+
     if (cTop < windowTop)
         $(window).scrollTop(cTop);
     else if (cTop + cHeight > windowTop + visibleHeight)
         $(window).scrollTop(cTop - visibleHeight + cHeight);
 };
+
 function selectItem(item) {
     listItems.removeClass("RES-active-list-item");
     item.addClass("RES-active-list-item");
     currentlySelected = item;
     item.scrollMinimal();
 }
+
 var listItems = $("[name=summary-list-item]");
+
 // Select top item
 if (listItems.length > 0) {
     selectItem(listItems.first());
     var currentlySelected = listItems.first();
 }
+
 // Remove grey background on premium listings. Otherwise, my active item doesn't
 // stand out against such listings
 $(".premium").css("background", "white");
+
 listItems.click(function () {
     selectItem($(this));
 });
-$(window).bind('keyup', function (e) {
+
+$(window).bind('keyup', function(e) {
     var code = e.keyCode || e.which;
     if (code == 72) {
         // h
@@ -48,45 +58,39 @@ $(window).bind('keyup', function (e) {
         var prevPage = currentPage.prev();
         if (prevPage.length == 1)
             window.location = prevPage.find("a").attr("href");
-    }
-    else if (code == 74) {
+    } else if (code == 74) {
         // j
         if (typeof currentlySelected !== "undefined") {
             var next = currentlySelected.next("[name=summary-list-item]");
             if (next.length == 1)
                 selectItem(next);
         }
-    }
-    else if (code == 75) {
+    } else if (code == 75) {
         // k
         if (typeof currentlySelected !== "undefined") {
             var prev = currentlySelected.prev("[name=summary-list-item]");
             if (prev.length == 1)
                 selectItem(prev);
         }
-    }
-    else if (code == 76) {
+    } else if (code == 76) {
         // l
         var currentPage = $("#sliderBottom .current").parent();
         var nextPage = currentPage.next().find("a");
         if (nextPage.length == 1)
             window.location = nextPage.attr("href");
-    }
-    else if (code == 13) {
+    } else if (code == 13) {
         // enter
         if (typeof currentlySelected !== "undefined") {
             var url = currentlySelected.find(".price-new a").attr("href");
             window.open(url, "_blank"); // TODO: avoid popup blockers?
         }
-    }
-    else if (code == 221) {
+    } else if (code == 221) {
         // ]
         var currentThumb = $(".js-gallery-thumbnail.selected");
         var nextThumb = currentThumb.next(".js-gallery-thumbnail");
         if (nextThumb.length == 1)
             nextThumb.click();
-    }
-    else if (code == 219) {
+    } else if (code == 219) {
         // [
         var currentThumb = $(".js-gallery-thumbnail.selected");
         var prevThumb = currentThumb.prev(".js-gallery-thumbnail");
