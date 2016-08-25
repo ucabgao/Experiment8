b07b842b8e4557e73b7e928f9e4aa5b221e41d76
simon.gaozheng <simon.gaozheng@gmail.com>
2016-08-25 06:59:20 +0100

    annotation

--- adlnet/xAPIWrapper/xapiwrapper.js
+++ adlnet/xAPIWrapper/xapiwrapper.js
@@ -1,3 +1,5 @@
+/* @flow */
+
 // adds toISOString to date objects if not there
 // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
 if ( !Date.prototype.toISOString ) {
